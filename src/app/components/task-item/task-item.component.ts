456c03ed6b76555c7757e857b5569ef3a9ca7ae5
Luz Elena Galindo <luz.galindo@perficient.com>
2023-09-15 12:06:50 -0600

    update task

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -11,7 +11,7 @@ export class TaskItemComponent implements OnInit {
   @Input()
   task!: Task;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
-
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() { }
@@ -22,5 +22,9 @@ export class TaskItemComponent implements OnInit {
 
   onDelete(task: any) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: any) {
+    this.onToggleReminder.emit(task);
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +23,12 @@ export class TaskService {
   }
 
   deleteTask(task: Task): Observable<Task> {
-    const url = `${this.apiUrl}/${task.id}`
+    const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
   }
 }
