7b9668151211b90ef52b4a9b8da227cb43766c8b
Luz Elena Galindo <luz.galindo@perficient.com>
2023-09-15 11:21:33 -0600

    ignore and tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
 
 
 @Component({
@@ -8,6 +8,12 @@ import { TASKS } from '../mock-tasks';
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
 }
